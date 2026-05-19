604d34e7854ede715684b2f828d5151bcb501dcb
tehrim12 <stehrim26@gmail.com>
2026-05-19 13:44:49 +0530

    baud_gen.v

--- baud_gen.v
+++ baud_gen.v
@@ -1,16 +1,30 @@
-`timescale 1ns / 1ns
-module baud #(parameter freq=50000000,baudr=2400)(
-    input sys_clk,
-    input sys_rst,
-    output reg uart_clk
-    );
-    localparam endcount=(freq/(baudr*16*2));
-    reg [$clog2(endcount):0] count=0;
-    always @(posedge sys_clk or negedge sys_rst)
-        begin   
-            if(!sys_rst) begin uart_clk <=0;count<=0; end
-            else if(count == endcount-1) begin uart_clk <= ~uart_clk;count<=0;end
-            else count <= count+1;
-        end       
+module baud #(parameter clk_freq=5000000,baud_rate=2400)(
+	input sys_clk,
+	input sys_rst,
+	output reg baud_op_clk);
+	
+	integer count;
+	
+	parameter count1= (clk_freq)/(16*baud_rate);
+	
+	always @(posedge sys_clk or negedge sys_rst)
+     	begin
+			if(!sys_rst) begin
+			baud_op_clk<=0;
+			count<=0;
+			end
+			
+			else if (count==(count1-1))
+			begin
+			baud_op_clk<=~baud_op_clk;
+			count<=0;
+			end
+			
+			else
+			begin
+			baud_op_clk<=0;
+			count<=count+1;
+			end
+			
+		end
 endmodule
-
